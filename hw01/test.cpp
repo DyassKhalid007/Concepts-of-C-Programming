bea578ad88bdf1bf9a585c45ebff930f4af9f891
Dyass Khalid <dyasskhalid@DESKTOP-MS8MTNH.localdomain>
2022-10-22 00:13:59 +0200

    Build the file finally

--- hw01/test.cpp
+++ hw01/test.cpp
@@ -1,10 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include "library.h"
-#include "doctest/doctest/doctest.h"
+#include <doctest/doctest.h>
 
 
 
 TEST_CASE("testing the library") {
     CHECK(library_function() == 42);
 }
+
+
